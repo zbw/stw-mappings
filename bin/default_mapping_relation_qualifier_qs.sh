d45a8d313c1eef4c6e258b7b0cd08c43650dc0bd
Joachim Neubert <j.neubert@zbw.eu>
2019-07-04 15:36:40 +0200

    Fix changed query name

--- bin/default_mapping_relation_qualifier_qs.sh
+++ bin/default_mapping_relation_qualifier_qs.sh
@@ -4,5 +4,5 @@
 # create quickstatements which set all empty mapping relation qualifiers
 # for a property to a default (as defined in the query file)
 
-perl /opt/sparql-queries/bin/make_qs_input.pl /opt/sparql-queries/wikidata/mapping_relation_qualifier_qs2.rq qs2
+perl /opt/sparql-queries/bin/make_qs_input.pl /opt/sparql-queries/wikidata/mapping_relation_qualifier_qs.rq qs2
 
